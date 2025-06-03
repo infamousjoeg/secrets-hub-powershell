167e2834da16d05c2c86d0e42c21562fb3a3a576
Joe Garcia <infamousjoeg@users.noreply.github.com>
2025-06-03 17:27:03 -0400

    Create Remove-SecretStore.ps1

---
+++ Public/SecretStores/Remove-SecretStore.ps1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+Removes a secret store from Secrets Hub.
+
+.DESCRIPTION
+Deletes the specified secret store. Note that linked policies must be deleted first.
+
+.PARAMETER StoreId
+The unique identifier of the secret store to remove.
+
+.PARAMETER Force
+Suppress confirmation prompts.
+
+.EXAMPLE
+Remove-SecretStore -StoreId "store-5a05468b-fa58-4bcf-84e9-62ede8af55f4"
+
+.NOTES
+All policies using this secret store must be deleted first.
+#>
+function Remove-SecretStore {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias('Id')]
+        [string]$StoreId,
+        
+        [Parameter()]
+        [switch]$Force
+    )
+    
+    begin {
+        Test-SecretsHubConnection
+    }
+    
+    process {
+        try {
+            if ($Force -or $PSCmdlet.ShouldProcess($StoreId, "Remove Secret Store")) {
+                $Uri = "api/secret-stores/$StoreId"
+                Invoke-SecretsHubApi -Uri $Uri -Method DELETE
+                Write-Host "Successfully removed secret store: $StoreId" -ForegroundColor Yellow
+            }
+        }
+        catch {
+            Write-SecretsHubError -ErrorRecord $_ -Operation "Remove-SecretStore"
+            throw
+        }
+    }
+}
