6d7cef316e794db32010d665d8aba352d7ae6f34
Joe Garcia <infamousjoeg@users.noreply.github.com>
2025-06-03 17:27:46 -0400

    Create Disable-SecretStore.ps1

---
+++ Public/SecretStores/Disable-SecretStore.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+Disables a secret store in Secrets Hub.
+
+.DESCRIPTION
+Changes the state of a secret store to DISABLED.
+
+.PARAMETER StoreId
+The unique identifier of the secret store to disable.
+
+.EXAMPLE
+Disable-SecretStore -StoreId "store-5a05468b-fa58-4bcf-84e9-62ede8af55f4"
+#>
+function Disable-SecretStore {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias('Id')]
+        [string]$StoreId
+    )
+    
+    begin {
+        Test-SecretsHubConnection
+    }
+    
+    process {
+        try {
+            if ($PSCmdlet.ShouldProcess($StoreId, "Disable Secret Store")) {
+                $Uri = "api/secret-stores/$StoreId/state"
+                $Body = @{ action = "disable" }
+                
+                Invoke-SecretsHubApi -Uri $Uri -Method PUT -Body $Body
+                Write-Host "Successfully disabled secret store: $StoreId" -ForegroundColor Yellow
+            }
+        }
+        catch {
+            Write-SecretsHubError -ErrorRecord $_ -Operation "Disable-SecretStore"
+            throw
+        }
+    }
+}
